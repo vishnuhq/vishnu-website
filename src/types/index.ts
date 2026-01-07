699d3ec2e05d738d1db9baf3eb43f23679ec30ec
Vishnu Vardhan Putta <421pvv@gmail.com>
2026-01-06 19:45:00 -0500

    add typescript interfaces

---
+++ src/types/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Type definitions for the portfolio website
+ */
+
+/**
+ * Navigation item
+ */
+export interface NavItem {
+  /** Display label */
+  label: string;
+  /** Route path */
+  href: string;
+}
